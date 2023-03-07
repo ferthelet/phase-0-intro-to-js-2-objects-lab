5e57a8367fd750971e4ae5ad4e825f9d0176431c
Juan Salazar <ferthelet@gmail.com>
2023-03-07 12:38:50 -0600

    adds first non destructive update

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+// js
+
+const employee = {
+    name : "John",
+    streetAddress : "939 Rodney Street"
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    // returns an employee with the original key value pairs
+    // and the new key value pair
+    // it does not modify the original employee, but rather
+    // returns a clone with the new data
+    return {...obj, [key]: value };
+}
+
+
+
